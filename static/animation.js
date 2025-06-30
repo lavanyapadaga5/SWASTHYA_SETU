3872f8aaa814b6053fb534edf2a636e48314dd55
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-06-30 16:01:59 +0530

    Add files via upload

---
+++ static/animation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dashboard = document.querySelector(".dashboard");
+    if(dashboard) {
+        dashboard.classList.add("fade-in");
+    }
+});
